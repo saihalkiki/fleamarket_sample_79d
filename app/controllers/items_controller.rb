74d0dd747096f98b983c6f4c2797a1c50dd09953
radwimpsyakisoba <26radwimps@gmail.com>
2020-07-24 20:08:42 +0900

    Data acquisition process in items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   def index
+    @items_new = Item.all.order("created_at DESC")
+    @items_archive = Item.all
   end
   def show
     @item=Item.find(params[:id])
