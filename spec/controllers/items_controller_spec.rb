ed5054d8cef669b6b3d725f31b9cc6f66553cd6b
halkity <saito75061@gmail.com>
2020-07-24 20:49:16 +0900

    Update items_controller_spec.rb

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,16 +1,16 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe ItemsController, type: :controller do
-#   describe 'GET #index' do
-#     it "indexのviewが表示される" do
-#       get :index
-#       expect(response).to render_template :index
-#     end
-#     it "@itemsにデータを取得できているか" do
-#       item = create(:item)
-#       get :index
-#       expect(assigns(:item)).to eq item
-#     end
-#   end
+describe ItemsController, type: :controller do
+  describe 'GET #index' do
+    it "indexのviewが表示される" do
+      get :index
+      expect(response).to render_template :index
+    end
+    # it "@itemsにデータを取得できているか" do
+    #   item = create(:item)
+    #   get :index
+    #   expect(assigns(:item)).to eq item
+    # end
+  end
 
-# end
+end
