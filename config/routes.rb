546d9b2a5fed876f57b16b1e216220bdf32078c2
halkity <saito75061@gmail.com>
2020-07-24 15:05:39 +0900

    added bunner layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'items#show'
+  root 'items#index'
   resources :items, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
