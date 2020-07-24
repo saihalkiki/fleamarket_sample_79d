495adfb293f0ac943d8777d68d47def1c37a8455
radwimpsyakisoba <26radwimps@gmail.com>
2020-07-24 20:11:30 +0900

    Creating sample data for testing

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :item do
-    
+    name            {"カバン"}
+    explanation     {"シャネルの高級バックです"}
+    quality         {良}
+    delivery_cost   {500}
+    region          {"東京都"}
+    period          {10}
+    price           {10000}
   end
 end
