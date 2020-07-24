ce1e934bd1256f9c5157cffecca2c5478d41c17f
radwimpsyakisoba <26radwimps@gmail.com>
2020-07-24 20:34:54 +0900

    WIP index rspec test

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name            {"カバン"}
     explanation     {"シャネルの高級バックです"}
-    quality         {良}
+    quality         {"良"}
     delivery_cost   {500}
     region          {"東京都"}
     period          {10}
