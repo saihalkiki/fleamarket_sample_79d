fa2c1e1d5ab95dc3f4d2e028685e9198f6162ca6
radwimpsyakisoba <26radwimps@gmail.com>
2020-07-26 19:25:42 +0900

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
-    resources :items, only: [:index, :new, :create, :show, :edit] do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update] do
       resources :images, only: [:new, :create]
     end
   end
