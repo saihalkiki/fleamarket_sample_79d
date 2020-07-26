9d5c1a0750fd11b58fcb1d1e0e9ba646bc183c10
radwimpsyakisoba <26radwimps@gmail.com>
2020-07-26 19:27:49 +0900

    Describe the update action to the controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,11 @@ class ItemsController < ApplicationController
     @item=Item.find(params[:id])
   end
 
+  def update
+    item= Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :quality, :delivery_cost, :region, :period, :price, :user)
