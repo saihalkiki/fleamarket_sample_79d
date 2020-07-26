0aab14150e2aa21484834c81697fb7ecbd70fb69
radwimpsyakisoba <26radwimps@gmail.com>
2020-07-26 15:29:42 +0900

    Additional editi features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
-    resources :items, only: [:index, :new, :create, :show] do
+    resources :items, only: [:index, :new, :create, :show, :edit] do
       resources :images, only: [:new, :create]
     end
   end
