7e3e3aa6a19cda13f65586b56690ac6de0ec6ce5
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-27 17:54:18 +0900

    add configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   private
 
   def basic_auth
@@ -12,6 +14,13 @@ class ApplicationController < ActionController::Base
 
   def production?
     Rails.env.production?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name,
+                                      :last_name_read, :first_name_read, :gender, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :last_name, :first_name,
+                                      :last_name_read, :first_name_read, :gender, :birthday])
   end
 
 end
