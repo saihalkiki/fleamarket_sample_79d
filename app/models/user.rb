d405190f090f150cefb26f57d72f82be0ed5e87d
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-27 11:09:22 +0900

    create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  enum gender: { 男性: 0, 女性: 1}
+  validates :password, length: { minimum: 7 }
+  validates :nickname, presence: true, uniqueness: true
+
+  [:nickname, :last_name, :first_name, :last_name_read, :first_name_read,:gender, :birthday, :email ,:password].each do |v|
+    validates v, presence: true
+  end
 end
