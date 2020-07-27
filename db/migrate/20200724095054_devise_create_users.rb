ba588af86086da360fb20ebcb788170812dd2e51
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-27 11:09:06 +0900

    create db user

--- db/migrate/20200724095054_devise_create_users.rb
+++ db/migrate/20200724095054_devise_create_users.rb
@@ -4,6 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_read,     null: false
+      t.string :first_name_read,    null: false
+      t.integer :gender,            null: false, default: 0
+      t.date :birthday,             null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -36,6 +43,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
