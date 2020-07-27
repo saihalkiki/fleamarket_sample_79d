07aa818df69bcf270a4e9859c6c6b22c6aaa16cc
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-27 12:32:22 +0900

    add valid user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
   enum gender: { 男性: 0, 女性: 1}
   validates :password, length: { minimum: 7 }
   validates :nickname, presence: true, uniqueness: true
+  # 全角ひらがな、全角カタカナ、漢字
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  [:last_name, :first_name].each do |v|
+    validates v, format: { with: VALID_NAME_REGEX }
+  end
+  #　全角ひらがな
+  VALID_NAME_READ_REGEX = /\A[ぁ-んー－]+\z/
+  [:last_name_read, :first_name_read].each do |v|
+    validates v, format: { with: VALID_NAME_READ_REGEX }
+  end
 
   [:nickname, :last_name, :first_name, :last_name_read, :first_name_read,:gender, :birthday, :email ,:password].each do |v|
     validates v, presence: true
