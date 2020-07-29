bd358a45c1e88585be078df7434dada5c8c9bdd6
halkity <saito75061@gmail.com>
2020-07-29 15:13:15 +0900

    added ajax category routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-    resources :items, only: [:index, :new, :create, :show, :edit, :update] do
-      resources :images, only: [:new, :create]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :images, only: [:new, :create]
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
     end
   end
+end
  
