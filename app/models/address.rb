3fbbdf5657726147b2b885dbbfe5dd225870c1a3
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-29 17:25:24 +0900

    added association address-prefecture

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,7 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
   belongs_to :user, optional: true
 
   validates :phone_number, uniqueness: true
