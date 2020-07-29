60655a5edd19ffa3fdd026a061895edeb85ce7d1
radwimpsyakisoba <26radwimps@gmail.com>
2020-07-29 16:41:37 +0900

    No change

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 2020_07_28_053701) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "prefecture_id", null: false
-    t.index ["prefecture_id"], name: "index_items_on_prefecture_id"
     t.bigint "user_id", null: false
+    t.index ["prefecture_id"], name: "index_items_on_prefecture_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
