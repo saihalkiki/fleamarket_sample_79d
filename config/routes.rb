7cb42d465f813c8393deeed67c4603aa6adee3f4
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-29 12:17:47 +0900

    Edit devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root 'items#index'
     resources :items, only: [:index, :new, :create, :show, :edit, :update] do
       resources :images, only: [:new, :create]
