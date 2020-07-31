c0ac5cd924219ef5ebaa0d94c3436eb43c3adc6b
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-31 19:08:43 +0900

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_084244) do
+ActiveRecord::Schema.define(version: 2020_07_30_113532) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
