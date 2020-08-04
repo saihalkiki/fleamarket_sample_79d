d3944bbfe79a82cd48e4348f930718df8cda0cb9
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-04 15:29:23 +0900

    added search routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
+      get 'search'
     end
     resources :comments, only: :create
   end
