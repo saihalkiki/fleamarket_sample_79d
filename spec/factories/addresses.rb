7513a44916d056a08b1e0da2cb3fa1dc78ec5da9
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-06 20:01:20 +0900

    Create address factories

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_code       {"000-0000"}
+    prefecture_id     {"1"}
+    city              {"渋谷区"}
+    house_number      {"1-1"}
+    building_name     {"渋谷マンション"}
+    phone_number      {"08012345678"}
   end
 end
