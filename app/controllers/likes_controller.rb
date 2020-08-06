29047a1e7e01b01e1f4717c906c1cb76d8be6f32
kohkoh22 <kohnosuke@gmail.com>
2020-08-06 12:35:41 +0900

    Updated like-function 3rd time

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,15 @@
 class LikesController < ApplicationController
   def create
+    @item = Item.find(params[:item_id])
     @like = current_user.likes.create(item_id: params[:item_id])
-    redirect_back(fallback_location: root_path)
+   
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @item = Item.find(params[:item_id])
     @like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -13,8 +14,6 @@ require('../category')
 require("../payjp")
 require('../new_item_ preview')
 require('../comment')
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
