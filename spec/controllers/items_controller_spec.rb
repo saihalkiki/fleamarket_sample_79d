9ab969dad4d1a99f094da667abd928404adf9e55
Takashi Aoki <arfaben_5816@icloud.com>
2020-08-07 21:23:42 +0900

    Update items_controller_spec.rb address.rb

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+describe ItemsController, type: :controller do
+
+  describe 'GET #new' do
+    it "出品ページに遷移するか" do
+      user = create(:user)
+      sign_in user
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    it "assigns the requested item to @item" do
+      item = create(:item)
+      get :edit, params: { id: item }
+      expect(assigns(:item)).to eq item
+    end
+
+    it "renders the :edit template" do
+      user = create(:user)
+      sign_in user
+      item = create(:item)
+      get :edit, params: { id: item }
+      expect(response).to render_template :edit
+    end
+  end
+
+  # describe 'GET #index' do
+  #   it "populates an array of items ordered by created_at DESC" do
+  #     items = create_list(:item, 3)
+  #     get :index
+  #     expect(assigns(:items)).to match(items.sort{ |a, b| b.created_at <=> a.created_at })
+  #   end
+
+  #   it "renders the :index template" do
+  #     get :index
+  #     expect(response).to render_template :index
+  #   end
+  # end
+end
