8d14c8a91e69c52d2dadeada576026eeecf8c8e5
halkity <saito75061@gmail.com>
2020-08-07 19:15:23 +0900

    omniauthを使うためのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
-
+  has_many :sns_credentials
   has_many :items
   has_one :address
   has_many :comments
