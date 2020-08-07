def10574159806362c013b17217eac86bd241477
halkity <saito75061@gmail.com>
2020-08-07 19:16:55 +0900

    omniauth_callbacksのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
