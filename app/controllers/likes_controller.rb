1d0da1d2242a66fe7f78c853c1a381d73e4d4de1
kohkoh22 <kohnosuke@gmail.com>
2020-08-07 19:34:42 +0900

    Updated likes-function 4th time

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,14 @@
 class LikesController < ApplicationController
   before_action :set_item, only: [:create, :destroy]
   def create
-    @item = Item.find(params[:item_id])
     @like = current_user.likes.create(item_id: params[:item_id])
-   
-    # redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
     @like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
     @like.destroy
-    # redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   private
