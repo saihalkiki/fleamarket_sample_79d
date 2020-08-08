17f774f8aa7d14e022714e2dae09346661e693e7
halkity <saito75061@gmail.com>
2020-08-08 16:37:48 +0900

    from_omniauth(auth)メソッドの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,22 @@ class User < ApplicationRecord
     self.likes.exists?(item_id: item.id)
   end
 
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
+
   enum gender: { 男性: 0, 女性: 1}
   validates :password, length: { minimum: 7 }
   validates :nickname, presence: true, uniqueness: true
