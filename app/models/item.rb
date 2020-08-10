851a2a431d45d2fb0a54a14bd3bbed6252c9cef9
radwimpsyakisoba <26radwimps@gmail.com>
2020-08-10 17:06:05 +0900

    items show correction(修正)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,5 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :period
-
+  
   belongs_to :category
   belongs_to :user
   has_many :images
@@ -11,6 +7,11 @@ class Item < ApplicationRecord
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
   accepts_nested_attributes_for :images, allow_destroy: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :period
+
+  enum delivery_cost:{"送料込み（出品者負担）": 1, "着払い（購入者負担）": 2}
 
   [:name, :explanation, :quality, :delivery_cost, :prefecture_id, :period_id, :price, :user_id].each do |v|
     validates v, presence: true
