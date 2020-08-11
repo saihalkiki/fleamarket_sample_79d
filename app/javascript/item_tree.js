748501c1e26401a3e6eee27b0fdecd97d2ff3fe8
halkity <saito75061@gmail.com>
2020-08-11 21:13:16 +0900

    upsate

--- app/javascript/item_tree.js
+++ app/javascript/item_tree.js
@@ -5,8 +5,20 @@ $(function () {
     $(this).children().removeClass('displayNone');
   },function(){
     let hideMenu = $(this).children('div')
-      setTimeCategoryTree = setTimeout(function() {
-        hideMenu.addClass('displayNone');
+    setTimeCategoryTree = setTimeout(function() {
+      hideMenu.addClass('displayNone');
     },200)
   })
+  $('.categoryTreeParent').hover(function(){
+    let parentId = $(this).children().data('index');
+    $.ajax({
+      url: '/items/get_category_children',
+      type: 'GET',
+      data: { parent_id: parentId },
+      dataType: 'json'
+    })
+    .done(function (children){
+      
+    })
+  },function(){})
 })
