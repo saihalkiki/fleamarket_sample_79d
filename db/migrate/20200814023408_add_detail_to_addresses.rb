96709827fbf958843fa7f3701336028780f6f8dd
kohkoh22 <kohnosuke@gmail.com>
2020-08-14 11:38:37 +0900

    Updated device-address-adjustment 1st time

---
+++ db/migrate/20200814023408_add_detail_to_addresses.rb
@@ -0,0 +1,8 @@
+class AddDetailToAddresses < ActiveRecord::Migration[6.0]
+  def change
+    add_column :addresses, :nickname, :string
+    add_column :addresses, :last_name, :string
+    add_column :addresses, :first_name, :string
+    add_column :addresses, :last_name_read, :string
+  end
+end
